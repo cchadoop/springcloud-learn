02b876de18685fc146372fd2621de3601855b9ca
smart <smart@onTop.com>
2019-09-12 11:53:33 +0800

    ribbon has hystrix function

--- springcloud-eureka-service-ribbon/src/main/java/com/ontop/controller/HelloController.java
+++ springcloud-eureka-service-ribbon/src/main/java/com/ontop/controller/HelloController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.ontop.service.HelloService;
 
 @RestController
@@ -13,7 +14,13 @@ public class HelloController {
 	HelloService helloService;
 	
 	@GetMapping(value="/hi")
+	//Hystrix 容错管理组件，实现了熔断器
+	@HystrixCommand(fallbackMethod="hiError")
 	public String hi(@RequestParam String name) {
 		return helloService.hiService(name);
+	}
+	
+	public String hiError(String name) {
+		return "hi,"+name+",sorry,error!";
 	}
 }
